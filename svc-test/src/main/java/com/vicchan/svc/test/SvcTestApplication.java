868278ab8fcfe904d2808748017140cefaf250ab
VicChanS <2734025186@qq.com>
2019-09-19 17:35:04 +0800

    添加配置中心

--- svc-test/src/main/java/com/vicchan/svc/test/SvcTestApplication.java
+++ svc-test/src/main/java/com/vicchan/svc/test/SvcTestApplication.java
@@ -3,6 +3,8 @@ package com.vicchan.svc.test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableDiscoveryClient
+@RefreshScope
 public class SvcTestApplication {
 
   public static void main(String[] args) {
@@ -20,9 +24,17 @@ public class SvcTestApplication {
   @Value("${server.port}")
   String port;
 
+  @Value("${foo}")
+  String foo;
+
   @RequestMapping("/hi")
   public String home(@RequestParam(value = "name", defaultValue = "VicChan") String name) {
     return "hi " + name + " ,i am from port:" + port;
+  }
+
+  @RequestMapping(value = "/foo")
+  public String hi(){
+    return foo;
   }
 
 }
