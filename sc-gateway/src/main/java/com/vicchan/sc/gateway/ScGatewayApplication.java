e356c600426d1b64fef4f747f005bf89535b8805
VicChanS <2734025186@qq.com>
2019-10-09 16:06:34 +0800

    把svc-test和sc-gateway的配置移至配置中心

--- sc-gateway/src/main/java/com/vicchan/sc/gateway/ScGatewayApplication.java
+++ sc-gateway/src/main/java/com/vicchan/sc/gateway/ScGatewayApplication.java
@@ -2,10 +2,14 @@ package com.vicchan.sc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class ScGatewayApplication {
 
   public static void main(String[] args) {
