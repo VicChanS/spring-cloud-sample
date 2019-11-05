584aa786b346a53a25afd43ae214988f8e230dc2
VicChanS <2734025186@qq.com>
2019-11-05 11:39:55 +0800

    添加components的高可用配置文件

--- components/docker-rabbitmq/cluster-entrypoint.sh
+++ components/docker-rabbitmq/cluster-entrypoint.sh
@@ -9,7 +9,7 @@ set -e
 
 # Do the cluster dance
 rabbitmqctl stop_app
-rabbitmqctl join_cluster rabbit@rabbitmq1
+rabbitmqctl join_cluster rabbit@rabbitmq
 
 # Stop the entire RMQ server. This is done so that we
 # can attach to it again, but without the -detached flag
